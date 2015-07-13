7591f320cdbb550e15027ef235d90c765d46ce22
Ronnie Nigash <nigash@uw.edu>
2015-07-13 09:05:37 -0700

    Add input file button

--- app.py
+++ app.py
@@ -1,27 +1,35 @@
+#from Tkinter import *
 import Tkinter
+from tkFileDialog import askopenfilename
 import sys
 
 class App(Tkinter.Tk):
-	
+
 
 
 	def __init__(self):
 		Tkinter.Tk.__init__(self)
 		toolbar = Tkinter.Frame(self)
-		toolbar.pack(side="top", fill="x")
+		toolbar.pack(side = "top", fill = "x")
 
 		button1 = Tkinter.Button(self, text = "Find matches", command = self.find)
-		
-		button1.pack(in_=toolbar, side = "top")
+		button2 = Tkinter.Button(self, text = "Input image", command = self.openfile)
+
+		button1.pack(in_ = toolbar, side = "top")
+		button2.pack(in_ = toolbar, side = "right")
 
 		self.text = Tkinter.Text(self, wrap = "word")
 		self.text.pack(side = "top", fill = "x", expand = True)
-	
+
+	def openfile(self):
+		print askopenfilename()
+
+
 
 	def find(self):
 		# @TODO
 		pass
-	
+
 	def close(self):
 		# @TODO
 		pass
