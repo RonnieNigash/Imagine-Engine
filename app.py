fe9521de86038cca8bf8b9fce544740ddfbce5e5
Ronnie Nigash <nigash@uw.edu>
2015-07-14 11:32:46 -0700

    Add labels, button, open_image function

--- app.py
+++ app.py
@@ -3,7 +3,25 @@ import Tkinter as tk
 class app(tk.Frame):
 	def __init__(self, parent, *args, **kwargs):
 		tk.Frame.__init__(self, parent, *args, **kwargs)
-		
+
+		self.file_path_label = tk.Label(parent, text = "Image File Path")
+		self.file_path_label.pack()
+
+		self.file_path = tk.Entry(parent, justify = tk.LEFT)
+		self.file_path.pack()
+		self.file_path.insert(0, "")
+
+		self.image = tk.Label(parent, text = "Image")
+		self.image.pack()
+
+		self.get_image = tk.Button(parent, text = "Input Image", command = open_image)
+		self.get_image = tk.Entry(parent, justify = tk.LEFT)
+		self.get_image.pack()
+		self.get_image.insert(0, "")
+
+def open_image(self):
+	filename = tk.askopenfilename(filetypes = [("jpgs", "*.jpg"), ("pngs","*.png"), ("gifs", "*.gifs"), ("allfiles", "*")] )
+	print filename
 
 if __name__ == "__main__":
 	root = tk.Tk()
