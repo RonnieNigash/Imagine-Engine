26e2d21ee8284c39e7a0991d95bc0bbeb6d106d7
Ronnie Nigash <nigash@uw.edu>
2015-07-14 11:51:48 -0700

    Fix imports for system file dialog

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import Tkinter as tk
+from tkFileDialog import askopenfilename
 from PIL import Image, ImageTk
+
 
 class app(tk.Frame):
 	def __init__(self, parent, *args, **kwargs):
@@ -15,18 +17,18 @@ class app(tk.Frame):
 		self.image = tk.Label(parent, text = "Image")
 		self.image.pack()
 
-		self.get_image = tk.Button(parent, text = "Input Image", command = open_image)
+		self.get_image = tk.Button(parent, text = "Input Image", command = self.open_image)
 		self.get_image.pack()
 
-def open_image(self):
-	filename = tk.askopenfilename(filetypes = [("jpgs", "*.jpg"), ("pngs","*.png"), ("gifs", "*.gifs"), ("allfiles", "*")] )
-	print filename
-	self.file_path.delete(1.0, END)
-	self.file_path.insert(0, filename)
+	def open_image(self):
+		filename = askopenfilename(filetypes = [("jpgs", "*.jpg"), ("pngs","*.png"), ("gifs", "*.gifs"), ("allfiles", "*")] )
+		print filename
+		self.file_path.delete(1.0, END)
+		self.file_path.insert(0, filename)
 
-	img = tk.PhotoImage(Image.open(filename))
-	self.image.delete(1.0, END)
-	self.image.insert(0, img)
+		img = tk.PhotoImage(Image.open(filename))
+		self.image.delete(1.0, END)
+		self.image.insert(0, img)
 
 if __name__ == "__main__":
 	root = tk.Tk()
