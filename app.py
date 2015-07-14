326874def9a2d8d81412522ba8ba0d2811cb6c8a
Ronnie Nigash <nigash@uw.edu>
2015-07-14 11:09:15 -0700

    Clear out

--- app.py
+++ app.py
@@ -1,47 +1,12 @@
-#!/usr/bin/python
-from Tkinter import *
-from tkFileDialog import *
-class App:
-	def __init__(self, parent = Tk()):
-		self.mainWindow = parent
-		self.mainWindow.title("Image Engine")
-		self.makemenu()
-		self.makelabel()
-		self.makeimage()
+import Tkinter as tk
 
-	def makemenu(self):
-		self.menubar = Menu(self.mainWindow)
-		self.filemenu = Menu(self.menubar, tearoff = 0)
-		self.menubar.add_cascade(label = "File", menu = self.filemenu)
+class app(tk.Frame):
+	def __init__(self, parent, *args, **kwargs):
+		tk.Frame.__init__(self, parent, *args, **kwargs)
 		
-		self.filemenu.add_command(label = "Open", command = self.openfile)
-		self.filemenu.add_separator()
-		self.filemenu.add_command(label = "Exit", command = self.mainWindow.destroy)
 
-		self.mainWindow.config(menu = self.menubar)
-
-	def openfile(self):
-		filename = askopenfilename(filetypes = [("jpgs", "*.jpg"), ("pngs","*.png"), ("gifs", "*.gifs"), ("allfiles", "*")] )
-		print filename
-		self.text.delete(1.0, END)
-		self.text.insert(1.0, filename)
-
-	def makeimage(self):
-		pass
-		#filename = Entry(self.text)
-		#Image = PhotoImage(file = filename)
-
-	def makelabel(self):
-		self.text = Text(self.mainWindow, width = 60, height = 1, background = 'white')
-		self.text.pack(expand = TRUE, fill = BOTH)
-
-	def find(self):
-		# @TODO
-		pass
-
-	def close(self):
-		# @TODO
-		pass
-
-app = App()
-app.mainWindow.mainloop()
+if __name__ == "__main__":
+	root = tk.Tk()
+	app(root).pack(side = "top", fill = "both", expand = True)
+	root.title("Imagine Engine")
+	root.mainloop()
