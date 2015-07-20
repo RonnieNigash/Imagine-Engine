522d62b20c7fbf4088b9a0212af7c5ac5f869386
Ronnie Nigash <nigash@uw.edu>
2015-07-20 15:18:43 -0700

    Update models for correct imports

--- webUI/search/models.py
+++ webUI/search/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class InputImage(models.Model):
-	input_image_file_path = CharField(max_length = 50)
+	input_image_file_path = models.CharField(max_length = 50)
 
 class FoundImages(models.Model):
 	input_image = models.ForeignKey(InputImage)
-	output_image_file_path = CharField(max_length = 50)
+	output_image_file_path = models.CharField(max_length = 50)
