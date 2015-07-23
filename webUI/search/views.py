4d3b69501f75ee7e19566b4aa54321b143cbb8be
Ronnie Nigash <nigash@uw.edu>
2015-07-22 17:46:38 -0700

    Update forms to redirect document list after uploading image

--- webUI/search/views.py
+++ webUI/search/views.py
@@ -8,6 +8,17 @@ from django.http import HttpResponseRedirect
 from webUI.search.models import Image 
 from webUI.search.forms import ImageForm
 
+def list(request):
+	if request.method == "POST":
+		form = ImageForm(request.POST, request.FILES)
+		if form.is_valid():
+			newimg = Image(docfile = request.FILES["imgfile"])
+			newimg.save()
+
+			return HttpResponseRedirect(reverse("webUI.views.list"))
+		else:
+			form = ImageForm()
+
 def index(request):
 	return HttpResponse("Search index page.")
 
