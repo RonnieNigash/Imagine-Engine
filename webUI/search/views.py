795c526fbb276c446b75b7ab4fba8a829d0701e4
Ronnie Nigash <nigash@uw.edu>
2015-07-22 17:44:07 -0700

    Add imports to views to include new models and form

--- webUI/search/views.py
+++ webUI/search/views.py
@@ -1,5 +1,12 @@
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.core.urlresolvers import reverse
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+from webUI.search.models import Image 
+from webUI.search.forms import ImageForm
 
 def index(request):
 	return HttpResponse("Search index page.")
