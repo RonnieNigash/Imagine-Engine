4fc062c39b3b6a3ae8c64726790e4337af24433f
Ronnie Nigash <nigash@uw.edu>
2015-07-22 17:54:39 -0700

    Update urls to display proper view

--- webUI/search/urls.py
+++ webUI/search/urls.py
@@ -1,4 +1,5 @@
-from django.conf.urls import url
+from django.conf.urls import url, patterns
 from . import views
 
-urlpatterns = [ url(r'^$', views.index, name = 'index'), ]
+urlpatterns = patterns("webUI.views", url(r'^input/$', 'input', name = 'input'),)
+#urlpatterns = [ url(r'^$', views.index, name = 'index'), ]
