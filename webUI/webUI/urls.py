c7a5a6882f6f7cf175d999fc91a319336c27baef
Ronnie Nigash <nigash@uw.edu>
2015-07-25 20:41:58 -0700

    Update urls for URL pattern

--- webUI/webUI/urls.py
+++ webUI/webUI/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
 	url(r'^search/', include('search.urls')),
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 ]
