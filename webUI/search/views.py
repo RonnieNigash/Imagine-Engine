aff714d2310ccaf110b4cd8ade7e4bf3e899f646
Ronnie Nigash <nigash@uw.edu>
2015-07-25 20:38:00 -0700

    Update importing in views for correct directory structure

--- webUI/search/views.py
+++ webUI/search/views.py
@@ -5,8 +5,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 
-from webUI.search.models import Image 
-from webUI.search.forms import ImageForm
+#from webUI.search.models import Image 
+#from webUI.search.forms import ImageForm
+from search.models import Image
+from search.forms import ImageForm
 
 def list(request):
 	if request.method == "POST":
