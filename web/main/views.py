09e2bfeb22a264766b2a9c1c56541466896ea811
Ronnie Nigash <nigash@uw.edu>
2015-08-23 12:43:43 -0700

    Update class-based view for handling form

--- web/main/views.py
+++ web/main/views.py
@@ -1,1 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.views.generic import FormView, DetailView, ListView
+
+from .forms import ImageForm
+from .models import Image
+
+class ImageView(FormView):
+	template_name = 'main/uploaded_image_form.html'
+	form_class = ImageForm
+
+	def form_valid(self, form):
+		return HttpResponseRedirect(self.get_success_url())
+
+	def get_success_url(self):
+		pass
+
+
+class DetailView(DetailView):
+	model = Image
+	template_name = 'main/uploaded_image.html'
+	context_object_name = 'image'
+
+class ImageIndexView(ListView):
+	model = Image
+	template_name = 'main/uploaded_image_view.html'
+	context_object_name = 'images'
+	queryset = Image.objects.all()
