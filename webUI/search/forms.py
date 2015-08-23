8b5187515f1a7b7f2b8642e157e6164e4e5f3ac2
Ronnie Nigash <nigash@uw.edu>
2015-08-23 09:54:37 -0700

    Remove UI -- revert to CLI

--- webUI/search/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class ImageForm(forms.Form):
-	imgfile = forms.FileField(
-			label = 'Select an image')
--- webUI/search/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns, url
-from search import views
-
-urlpatterns = patterns('',
-		url(r'^$', views.input, name='input'),
-)
